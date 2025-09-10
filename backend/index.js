9c367d0ff1db4764784f67d9440c503fcb75636b
septiandwica <septiandwica03@gmail.com>
2025-09-10 14:13:12 +0700

    update cors

--- backend/index.js
+++ backend/index.js
@@ -16,50 +16,13 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(
   cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-        "https://learntubehub.vercel.app",
-        "http://localhost:5173",
-      ];
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        return callback(null, true); // temporarily allow all origins
-      }
-      return callback(null, true);
-    },
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "X-Requested-With",
-      "Accept",
-    ],
+    origin: `http://${process.env.CLIENT_URL}`,
     credentials: true,
-    maxAge: 86400, // 24 hours
   })
 );
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser());
-
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://learntubehub.vercel.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Requested-With, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 // Initialize passport
 app.use(passport.initialize());
@@ -86,7 +49,9 @@ connectDB()
     console.log("--- Server Started ---");
     console.log("MongoDB connection success");
     app.listen(PORT, () => {
-      console.log(`Server running`);
+      console.log(
+        `Server running`
+      );
       console.log("---- Logger ----");
     });
   })
